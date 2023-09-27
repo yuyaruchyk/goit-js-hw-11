97b525348bfea9d9104814d7fc634f3b2e50416b
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-27 13:29:58 +0200

    upd
    upd

--- src/index.js
+++ src/index.js
@@ -12,23 +12,20 @@ const refs = {
 const baseUrl = 'https://pixabay.com/api/';
 const apiKey = '39696630-76278aa8e60be2b194df9a30b';
 
-
-
-
 refs.form.addEventListener('submit', onSearch);
 
 function onSearch(e) {
     e.preventDefault();
     const searchQuery = e.currentTarget.elements.query.value;
-   
+    getImage(searchQuery); 
 }
 
-async function getImage() {
+async function getImage(searchQuery) {
   try {
       const response = await axios.get(`${baseUrl}?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
-      console.log(response)
-    console.log(response);
+      console.log(response.data); 
   } catch (error) {
     console.error(error);
   }
 }
+
