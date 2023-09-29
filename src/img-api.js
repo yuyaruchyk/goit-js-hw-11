56ef18bb4b81baa2ff325dbd9dc7ee811f01c7a9
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-29 23:09:22 +0200

    upd
    upd

--- src/img-api.js
+++ src/img-api.js
@@ -11,7 +11,7 @@ async function getImage(searchQuery) {
 const apiKey = '39696630-76278aa8e60be2b194df9a30b';
 
   try {
-      const response = await axios.get(`${baseUrl}?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
+      const response = await axios.get(`${baseUrl}?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`);
       return response.data; 
   } catch (error) {
     console.error(error);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { getImage } from './img-api'
 
+
+
 const refs = {
     form: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
@@ -12,12 +14,31 @@ const refs = {
 } 
 
 
+let currentPage = 1;
+
 refs.form.addEventListener('submit', onSearch);
 refs.loadMore.addEventListener('click', onLoadMore);
 
 
+function generateImageMarkup(imageInfo) {
+    return imageInfo.map(image => `
+        <div class="image-container">
+            <img class="main-image" src="${image.webformatURL}" alt="${image.tags}">
+            <div class="text-container"> 
+                <p class="main-text">Tags: ${image.tags}</p>
+                <p class="main-text">Likes: ${image.likes}</p>
+                <p class="main-text">Views: ${image.views}</p>
+                <p class="main-text">Comments: ${image.comments}</p>
+                <p class="main-text">Downloads: ${image.downloads}</p>
+            </div>
+        </div>
+    `).join('');
+}
+
 function onSearch(e) {
     e.preventDefault();
+
+    currentPage = 1;
 
     const searchQuery = refs.form.elements.searchQuery.value;
     getImage(searchQuery)
@@ -25,42 +46,43 @@ function onSearch(e) {
             const imageInfo = data.hits;
             console.log(imageInfo);
 
-            const markUp = imageInfo.map(image => `
-                <div class="image-container">
-                    <img class="main-image" src="${image.webformatURL}" alt="${image.tags}">
-                    <div class="text-container"> 
-                        <p class="main-text">Tags: ${image.tags}</p>
-                        <p class="main-text">Likes: ${image.likes}</p>
-                        <p class="main-text">Views: ${image.views}</p>
-                        <p class="main-text">Comments: ${image.comments}</p>
-                        <p class="secondary-title">Downloads: ${image.downloads}</p>
-                    </div>
-                </div>
-            `).join('');
+            if (imageInfo.length === 0) {
+                 Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            }
+
+            const markUp = generateImageMarkup(imageInfo);
 
             refs.gallery.innerHTML = markUp;
 
         })
         .catch(error => {
             console.error(error);
+            Notiflix.Notify.failure("An error occurred while fetching images. Please try again later.");
         });
 }
 
 
+function onLoadMore(e) {
+    e.preventDefault();
 
+    currentPage += 1;
 
-    
-
-
-  
-    
-
-
-function onLoadMore() {
-
-   const searchQuery = refs.form.elements.searchQuery.value;
-  getImage(searchQuery);
+    const searchQuery = refs.form.elements.searchQuery.value;
+    getImage(searchQuery)
+     .then(data => {
+         const additionalImageInfo = data.hits;
+           const markUp = generateImageMarkup(additionalImageInfo);
+          refs.gallery.innerHTML += markUp;
+      })
+     .catch(error => {
+         console.error(error);
+         Notiflix.Notify.failure("An error occurred while fetching images. Please try again later.");
+    });
 }
+    
+     
+        
+
 
 
 
