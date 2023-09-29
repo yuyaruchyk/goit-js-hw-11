3aa86ad9329136407415ecd199bc2d10e45ad146
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-29 21:27:09 +0200

    upd
    upd

--- src/img-api.js
+++ src/img-api.js
@@ -3,17 +3,25 @@ import axios from "axios";
 export { getImage };
 
     
-    const baseUrl = 'https://pixabay.com/api/';
-const apiKey = '39696630-76278aa8e60be2b194df9a30b';
+   
 
 async function getImage(searchQuery) {
+    
+ const baseUrl = 'https://pixabay.com/api/';
+const apiKey = '39696630-76278aa8e60be2b194df9a30b';
+
   try {
       const response = await axios.get(`${baseUrl}?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`);
-      console.log(response.data); 
+      return response.data; 
   } catch (error) {
     console.error(error);
   }
 }
+
+
+
+    
+ 
  
 
 
--- src/index.js
+++ src/index.js
@@ -12,20 +12,58 @@ const refs = {
 } 
 
 
-
 refs.form.addEventListener('submit', onSearch);
 refs.loadMore.addEventListener('click', onLoadMore);
 
+
 function onSearch(e) {
     e.preventDefault();
-    const searchQuery = e.currentTarget.elements.searchQuery.value;
-   getImage(searchQuery); 
+
+    const searchQuery = refs.form.elements.searchQuery.value;
+    getImage(searchQuery)
+        .then(data => {
+            const imageInfo = data.hits;
+            console.log(imageInfo);
+
+            const markUp = imageInfo.map(image => `
+                <div class="image-container">
+                    <img class="main-image" src="${image.webformatURL}" alt="${image.tags}">
+                    <div class="text-container"> 
+                        <p class="main-text">Tags: ${image.tags}</p>
+                        <p class="main-text">Likes: ${image.likes}</p>
+                        <p class="main-text">Views: ${image.views}</p>
+                        <p class="main-text">Comments: ${image.comments}</p>
+                        <p class="secondary-title">Downloads: ${image.downloads}</p>
+                    </div>
+                </div>
+            `).join('');
+
+            refs.gallery.innerHTML = markUp;
+
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
-function onLoadMore(e) {
-    e.preventDefault();
-     getImage(searchQuery); 
+
+
+
+    
+
+
+  
+    
+
+
+function onLoadMore() {
+
+   const searchQuery = refs.form.elements.searchQuery.value;
+  getImage(searchQuery);
 }
+
+
+
 
 
 
