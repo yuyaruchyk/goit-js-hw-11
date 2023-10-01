019ce32039b859f1e81cf3340c28bcc0d2e280ca
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-10-01 23:01:13 +0200

    upd
    upd

--- src/index.js
+++ src/index.js
@@ -35,8 +35,18 @@ function generateImageMarkup(imageInfo) {
     return imageInfo.map(image => `
 
 <div class="photo-card">
-  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" style="width: 300px; height: 200px; object-fit: cover"  />
-  <div class="info">
+
+ <a class="gallery__link link" href="${image.largeImageURL}">
+    <img
+      class="gallery__image"
+      src="${image.webformatURL}"
+      alt="${image.tags}" loading="lazy" style="width: 300px; height: 200px; object-fit: cover"
+       
+     
+
+  </a>
+
+      <div class="info">
     <p class="info-item">
       <b>Likes <span class="span">${image.likes}</span> </b>
     </p>
@@ -50,12 +60,16 @@ function generateImageMarkup(imageInfo) {
       <b>Downloads <span class="span">${image.downloads}</span> </b>
     </p>
   </div>
+  
 </div>
     
     `).join('');
 }
 
-
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 function onSearch(e) {
   e.preventDefault();
@@ -76,12 +90,13 @@ function onSearch(e) {
       if (imageInfo.length === 0) {
         refs.loadMore.classList.add('hidden');
         refs.form.reset();
-        Notiflix.Notify.failure("Sorry, there are no images matching your search. Please try again.");
+        Notiflix.Notify.failure("Sorry, there are no images matching your search.");
       }
 
       const markUp = generateImageMarkup(imageInfo);
 
-      refs.gallery.innerHTML = markUp;
+        refs.gallery.innerHTML = markUp;
+        lightbox.refresh();
     })
     .catch(error => {
       console.error(error);
@@ -97,14 +112,17 @@ function onSearch(e) {
 
 function onLoadMore(e) {
     getImageApi.getImage()
-        .then(data => {
-            const additionalImageInfo = data.hits;
+      .then(data => {
+        console.log(data)
+        const totalHits = data.totalHits;
+        const additionalImageInfo = data.hits;
+        console.log(data.hits)
 
-            if (additionalImageInfo.length === 0) {
+            if (additionalImageInfo.length === totalHits || additionalImageInfo.length === 0)  {
                 refs.form.reset();
                  refs.loadMore.classList.add('hidden');
 
-                Notiflix.Notify.failure("Sorry, there are no more images matching your search.");
+                Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
             } else {
                 const markUp = generateImageMarkup(additionalImageInfo);
                 refs.gallery.innerHTML += markUp;
@@ -113,7 +131,7 @@ function onLoadMore(e) {
         .catch(error => {
             console.error(error);
              refs.loadMore.classList.add('hidden');
-            Notiflix.Notify.failure("An error occurred while fetching images. Please try again later.");
+            Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
         });
     
 }
